66bd75535138311150921efef3ccb1f6255e3bab
Leon <pavlenkoleon@gmail.com>
2022-06-27 01:40:22 +0300

    adjust button working as intended

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const sketch_cont = document.querySelector(".sketch-container");
 const btn_size = document.querySelector(".btn-size");
 
 btn_size.addEventListener("click",()=>{
-    let resolution = prompt("Resize canvas leave blank to clear");
+    sketch_cont.innerHTML= "";
+
+
+    let resolution = parseInt(prompt("Resize canvas leave blank to clear"));
 
     if (resolution >= 64){
         resolution = 64;
     }
 
-    sketch_cont.innerHTML=""
+    if(isNaN(resolution)){
+        resolution = 16;
+    }
+    
     drawGrid(resolution);
 })
 
@@ -20,23 +26,27 @@ document.body.onmouseup = () =>{
     mouseDown = false;
 };
 
-function drawGrid(res){
+color = "aqua"
 
-    for(let i = 0 ; i < res ; i ++){
+function drawGrid(res = 16){
+
+    for(let i = 0 ; i < res ; i++){
         let row = document.createElement("div");
         row.classList.add('row');
-        row.setAttribute("style","flex : 1 1 auto; display : flex;");
+        row.setAttribute("style","flex : 1   auto; display : flex;");
     
-        for(let i = 0 ; i < res ; i ++){
+        for(let i = 0 ; i < res ; i++){
             let block = document.createElement("div");
             block.classList.add('block');
             block.setAttribute("style","flex : 1 1 auto");
     
             block.addEventListener("mouseenter",()=>{
                 if(mouseDown){
-                    block.classList.add("black");
+                    block.style.backgroundColor = "rgba(0,0,0,0.1)";
                 }
-                
+            })
+            block.addEventListener("mousedown",()=>{
+                block.style.backgroundColor = "rgba(0,0,0,0.1)";
             })
     
             row.appendChild(block);
@@ -44,7 +54,7 @@ function drawGrid(res){
     
         sketch_cont.appendChild(row);
     }
-    
+
 }
 
 drawGrid(16);
