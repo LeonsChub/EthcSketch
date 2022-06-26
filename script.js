8e95fbde84e079c849fcd983732508cfa5b9694c
Leon <pavlenkoleon@gmail.com>
2022-06-27 00:38:07 +0300

    resizing screen works as intended

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const sketch_cont = document.querySelector(".sketch-container");
+const btn_size = document.querySelector(".btn-size");
+
+btn_size.addEventListener("click",()=>{
+    let resolution = prompt("Resize canvas leave blank to clear");
+
+    if (resolution >= 64){
+        resolution = 64;
+    }
+
+    sketch_cont.innerHTML=""
+    drawGrid(resolution);
+})
 
 let mouseDown = false;
 document.body.onmousedown = () =>{
@@ -8,31 +20,31 @@ document.body.onmouseup = () =>{
     mouseDown = false;
 };
 
-let resolution = 64;
+function drawGrid(res){
 
-for(let i = 0 ; i < resolution ; i ++){
-
-    let row = document.createElement("div");
-    row.classList.add('row');
-    row.setAttribute("style","flex : 1 1 auto; display : flex;");
-
-    for(let i = 0 ; i < resolution ; i ++){
-        let block = document.createElement("div");
-        block.classList.add('block');
-        block.setAttribute("style","flex : 1 1 auto");
-
-        block.addEventListener("mouseenter",()=>{
-            if(mouseDown){
-                block.classList.add("black");
-            }
-            
-        })
-
-        row.appendChild(block);
-
-        
-
+    for(let i = 0 ; i < res ; i ++){
+        let row = document.createElement("div");
+        row.classList.add('row');
+        row.setAttribute("style","flex : 1 1 auto; display : flex;");
+    
+        for(let i = 0 ; i < res ; i ++){
+            let block = document.createElement("div");
+            block.classList.add('block');
+            block.setAttribute("style","flex : 1 1 auto");
+    
+            block.addEventListener("mouseenter",()=>{
+                if(mouseDown){
+                    block.classList.add("black");
+                }
+                
+            })
+    
+            row.appendChild(block);
+        }
+    
+        sketch_cont.appendChild(row);
     }
-
-    sketch_cont.appendChild(row);
+    
 }
+
+drawGrid(16);
